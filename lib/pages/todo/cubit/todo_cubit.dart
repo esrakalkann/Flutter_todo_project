5b133dc8460de839b02b8d500e0ad2ec064111a7
esrakalkann <esra.kalkan@ogr.iuc.edu.tr>
2025-07-14 15:47:01 +0300

    updated todo_cubit (not finished yet)

--- lib/pages/todo/cubit/todo_cubit.dart
+++ lib/pages/todo/cubit/todo_cubit.dart
@@ -1,12 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/local/todo_model.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 part 'todo_state.dart';
 
 class ToDoCubit extends Cubit<ToDoState> {
-  List<ToDoModel> _allTodos = []; // Tüm todoları sakla
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  //List<ToDoModel> _allTodos = []; // Tüm todoları sakla
 
   ToDoCubit() : super(ToDoInitial()) {
     loadTodos();
@@ -15,17 +16,31 @@ class ToDoCubit extends Cubit<ToDoState> {
   Future<void> loadTodos() async {
     emit(ToDoLoading());
     try {
-      List<ToDoModel> toDoList = [];
+      final QuerySnapshot = await _firestore.collection('todos').get();
+      final toDoList = QuerySnapshot.docs.map((doc) {
+        final data = doc.data();
+        return ToDoModel(
+          id: doc.id,
+          text: data['text'],
+          isCompleted: data['isCompleted'] ?? false,
+          description: data['description'],
+          dateTime: data['dateTime'] != null
+              ? (data['dateTime'] as Timestamp).toDate()
+              : null,
+        );
+      }).toList();
+
+      //List<ToDoModel> toDoList = [];
       // Simüle edilmiş veri yükleme, yapay 3 saniyelik gecikme
       // Burada gerçek bir API çağrısı veya veritabanı sorgusu yapılabilir
-      await Future.delayed(Duration(seconds: 3), () {
-        toDoList = [
-          ToDoModel(id: 1, text: 'deneme'),
-          ToDoModel(id: 2, text: 'Write first commit'),
-          ToDoModel(id: 3, text: 'Write second commit'),
-        ];
-      });
-      _allTodos = toDoList;
+      //await Future.delayed(Duration(seconds: 3), () {
+      //  toDoList = [
+      //   ToDoModel(id: 1, text: 'deneme'),
+      // ToDoModel(id: 2, text: 'Write first commit'),
+      // ToDoModel(id: 3, text: 'Write second commit'),
+      //  ];
+      //  });
+      //  _allTodos = toDoList;
       // Veriler yüklendikten sonra durumu güncelle
       emit(ToDoLoaded(toDoList));
     } catch (e) {
@@ -35,7 +50,20 @@ class ToDoCubit extends Cubit<ToDoState> {
     }
   }
 
-  void addTodo(String text,{String? description, DateTime? dateTime}) {
+  Future <void> addTodo(String text, {String? description, DateTime? dateTime}) async {
+    try {
+      await _firestore.collection('todos').add({
+        'text': text,
+        'isCompleted': false,
+        'description': description,
+        'dateTime': dateTime,
+      });
+      loadTodos();
+    } catch (e) {
+      emit(ToDoError('Unable to add task: $e'));
+    }
+
+    /*
     if (state is ToDoLoaded) {
       final newTodo = ToDoModel(
         id: DateTime.now().millisecondsSinceEpoch,
@@ -46,14 +74,23 @@ class ToDoCubit extends Cubit<ToDoState> {
       _allTodos = [..._allTodos, newTodo];
       emit(ToDoLoaded([..._allTodos]));
     }
+    */
   }
 
-  void deleteTodo(ToDoModel todo) {
+  void deleteTodo(ToDoModel todo) async {
     if (state is ToDoLoaded) {
       // Silme işlemi için mevcut todo listesinden todo'yu kaldır
-
+      /*
       _allTodos.removeWhere((t) => t.id == todo.id);
       emit(ToDoLoaded([..._allTodos]));
+      */
+
+      try {
+        await _firestore.collection('todos').doc(todo.id).delete();
+        loadTodos();
+      } catch (e) {
+        emit(ToDoError('Unable to delete task: $e'));
+      }
     }
   }
 
