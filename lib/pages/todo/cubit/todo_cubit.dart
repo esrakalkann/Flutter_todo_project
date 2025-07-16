be880ab91b0a2e4c34a68d5dbaaed81268478f4d
enesbaskayaa <enesbsky@gmail.com>
2025-07-16 12:13:31 +0300

    firebase firestore integration add.

--- lib/pages/todo/cubit/todo_cubit.dart
+++ lib/pages/todo/cubit/todo_cubit.dart
@@ -1,90 +1,57 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/local/todo_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 part 'todo_state.dart';
 
 class ToDoCubit extends Cubit<ToDoState> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  //List<ToDoModel> _allTodos = []; // Tüm todoları sakla
 
   ToDoCubit() : super(ToDoInitial()) {
     loadTodos();
   }
 
   Future<void> loadTodos() async {
+    log('Loading todos...');
     emit(ToDoLoading());
     try {
-      final QuerySnapshot = await _firestore.collection('todos').get();
-      final toDoList = QuerySnapshot.docs.map((doc) {
+      final querySnapshot = await _firestore.collection('todos').get();
+
+      final toDoList = querySnapshot.docs.map((doc) {
         final data = doc.data();
         return ToDoModel(
           id: doc.id,
           text: data['text'],
           isCompleted: data['isCompleted'] ?? false,
           description: data['description'],
-          dateTime: data['dateTime'] != null
-              ? (data['dateTime'] as Timestamp).toDate()
-              : null,
+          dateTime: data['dateTime'] != null ? (data['dateTime'] as Timestamp).toDate() : null,
         );
       }).toList();
-
-      //List<ToDoModel> toDoList = [];
-      // Simüle edilmiş veri yükleme, yapay 3 saniyelik gecikme
-      // Burada gerçek bir API çağrısı veya veritabanı sorgusu yapılabilir
-      //await Future.delayed(Duration(seconds: 3), () {
-      //  toDoList = [
-      //   ToDoModel(id: 1, text: 'deneme'),
-      // ToDoModel(id: 2, text: 'Write first commit'),
-      // ToDoModel(id: 3, text: 'Write second commit'),
-      //  ];
-      //  });
-      //  _allTodos = toDoList;
-      // Veriler yüklendikten sonra durumu güncelle
       emit(ToDoLoaded(toDoList));
     } catch (e) {
-      // Hata durumunda durumu güncelle
       emit(ToDoError('Veriler yüklenirken bir hata oluştu: $e'));
       return;
     }
   }
 
-  Future <void> addTodo(String text, {String? description, DateTime? dateTime}) async {
+  Future<void> addTodo(String text, {String? description, DateTime? dateTime}) async {
+    log('Adding todo: $text');
     try {
-      await _firestore.collection('todos').add({
-        'text': text,
-        'isCompleted': false,
-        'description': description,
-        'dateTime': dateTime,
-      });
-      loadTodos();
+      final docRef = await _firestore.collection('todos').add({'text': text, 'isCompleted': false, 'description': description, 'dateTime': dateTime});
+      log('Todo added: $text, id: ${docRef.id}');
+      await Future.delayed(const Duration(milliseconds: 300)); // Firestore'ın veriyi işlemesi için kısa bir bekleme
+      await loadTodos();
     } catch (e) {
+      log('Error adding todo: $e');
       emit(ToDoError('Unable to add task: $e'));
     }
-
-    /*
-    if (state is ToDoLoaded) {
-      final newTodo = ToDoModel(
-        id: DateTime.now().millisecondsSinceEpoch,
-        text: text,
-        description: description,
-        dateTime: dateTime,
-      );
-      _allTodos = [..._allTodos, newTodo];
-      emit(ToDoLoaded([..._allTodos]));
-    }
-    */
   }
 
   void deleteTodo(ToDoModel todo) async {
     if (state is ToDoLoaded) {
-      // Silme işlemi için mevcut todo listesinden todo'yu kaldır
-      /*
-      _allTodos.removeWhere((t) => t.id == todo.id);
-      emit(ToDoLoaded([..._allTodos]));
-      */
-
       try {
         await _firestore.collection('todos').doc(todo.id).delete();
         loadTodos();
@@ -94,13 +61,10 @@ class ToDoCubit extends Cubit<ToDoState> {
     }
   }
 
-  void searchTodos(String query) async{
+  void searchTodos(String query) async {
     emit(ToDoLoading());
     try {
-      final snapshot = await _firestore
-          .collection('todos')
-          .where('text', isGreaterThanOrEqualTo: query)
-          .get();
+      final snapshot = await _firestore.collection('todos').where('text', isGreaterThanOrEqualTo: query).get();
 
       final results = snapshot.docs.map((doc) {
         final data = doc.data();
@@ -109,69 +73,30 @@ class ToDoCubit extends Cubit<ToDoState> {
           text: data['text'],
           isCompleted: data['isCompleted'] ?? false,
           description: data['description'],
-          dateTime: data['dateTime'] != null
-              ? (data['dateTime'] as Timestamp).toDate()
-              : null,
+          dateTime: data['dateTime'] != null ? (data['dateTime'] as Timestamp).toDate() : null,
         );
       }).toList();
-            emit(ToDoLoaded(results));
+      emit(ToDoLoaded(results));
     } catch (e) {
       emit(ToDoError('Search error: $e'));
     }
   }
 
-
-  Future <void> toggleTask(String id, bool currentState) async{
-    try{
-      await _firestore.collection('todos').doc(id).update({
-        'isCompleted': !currentState,
-      });
+  Future<void> toggleTask(String id, bool currentState) async {
+    try {
+      await _firestore.collection('todos').doc(id).update({'isCompleted': !currentState});
       loadTodos();
-    }catch(e){
+    } catch (e) {
       emit(ToDoError('Unable to update: $e'));
-
     }
-
-
-    /*
-    if (state is ToDoLoaded) {
-      _allTodos = _allTodos.map((todo) {
-        if (todo.id == id) {
-          return todo.toggleCompletion();
-        }
-
-        return todo;
-      }).toList();
-      emit(ToDoLoaded(_allTodos));
-    }
-    */
   }
 
-  Future <void> editTodo(String id, String newText) async
-  {
-    try{
-
-      await _firestore.collection('todos').doc(id).update({
-        'text': newText,
-      });
+  Future<void> editTodo(String id, String newText) async {
+    try {
+      await _firestore.collection('todos').doc(id).update({'text': newText});
       loadTodos();
-    } catch(e){
+    } catch (e) {
       emit(ToDoError('Unable to update: $e '));
     }
-
-    //}
-
-    /*if (state is ToDoLoaded) {
-      _allTodos = _allTodos.map((todo)
-      {
-        if (todo.id == id) {
-          return todo.copyWith(text: newText);
-        }
-
-        return todo;
-      }).toList();
-      emit(ToDoLoaded([..._allTodos]));
-    }*/
-    
   }
 }
--- lib/pages/todo/subpages/add_todo.dart
+++ lib/pages/todo/subpages/add_todo.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_application_1/pages/todo/cubit/todo_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AddTaskDialog extends StatelessWidget {
+class AddTaskDialog extends StatefulWidget {
   const AddTaskDialog({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final TextEditingController _titleController = TextEditingController();
-    final TextEditingController _descController = TextEditingController();
-    DateTime? _selectedDateTime;
+  State<AddTaskDialog> createState() => _AddTaskDialogState();
+}
 
+class _AddTaskDialogState extends State<AddTaskDialog> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descController = TextEditingController();
+  DateTime? selectedDateTime;
+
+  @override
+  Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Add Task'),
       content: SingleChildScrollView(
@@ -18,63 +23,44 @@ class AddTaskDialog extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             TextField(
-              controller: _titleController,
+              controller: titleController,
               decoration: const InputDecoration(hintText: 'Task Title'),
             ),
             const SizedBox(height: 10),
             TextField(
-              controller: _descController,
+              controller: descController,
               decoration: const InputDecoration(hintText: 'details'),
             ),
             const SizedBox(height: 8),
             ElevatedButton(
               onPressed: () async {
-                final pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2020),
-                  lastDate: DateTime(2100),
-                );
+                final pickedDate = await showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2020), lastDate: DateTime(2100));
                 if (pickedDate != null) {
-                  final pickedTime = await showTimePicker(
-                    context: context,
-                    initialTime: TimeOfDay.now(),
-                  );
+                  final pickedTime = await showTimePicker(context: context, initialTime: TimeOfDay.now());
                   if (pickedTime != null) {
-                    _selectedDateTime = DateTime(
-                      pickedDate.year,
-                      pickedDate.month,
-                      pickedDate.day,
-                      pickedTime.hour,
-                      pickedTime.minute,
-                    );
+                    setState(() {
+                      selectedDateTime = DateTime(pickedDate.year, pickedDate.month, pickedDate.day, pickedTime.hour, pickedTime.minute);
+                    });
                   }
                 }
               },
-              child: const Text('Date and Time'),
+              child: Text(selectedDateTime == null ? 'Date and Time' : selectedDateTime.toString()),
             ),
           ],
         ),
       ),
       actions: [
+        TextButton(onPressed: () => Navigator.of(context).pop(), child: const Text('Cancel')),
         TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: const Text('Cancel'),
-        ),
-        TextButton(
-          onPressed: () {
-            final title = _titleController.text.trim();
-            final desc = _descController.text.trim();
+          onPressed: () async {
+            final title = titleController.text.trim();
+            final desc = descController.text.trim();
             if (title.isNotEmpty) {
-              context.read<ToDoCubit>().addTodo(
-                title,
-                description: desc,
-                dateTime: _selectedDateTime,
-              );
+              await context.read<ToDoCubit>().addTodo(title, description: desc, dateTime: selectedDateTime);
               Navigator.of(context).pop();
             }
           },
-          child: const Text('Add'),
+          child: const Text('Task Ekle'),
         ),
       ],
     );
